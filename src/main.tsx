fad106ca3acb2415cdf02880a369f1f6d2b6356d
nojiritakeshi <takesoccer08nine@ezweb.ne.jp>
2023-08-23 22:24:48 +0900

    chakra-themeついき

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
 import App from "./App.tsx";
 import "./index.css";
 import { BtnBlock } from "./ChakraBtnBlock.tsx";
 
+const theme = extendTheme({
+  colors: {
+    primary: "blue",
+    secondary: "red",
+    third: "green",
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       {/* <App /> */}
-      <Button>Button</Button>
+      <Button color={"primary"} bg={"third"} name="BTN">
+        Button
+      </Button>
       <BtnBlock />
     </ChakraProvider>
   </React.StrictMode>
